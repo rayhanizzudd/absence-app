a13d873e265adb227cca00f64addd3b22a141e7e
rayhanizzudd <rayhannavis11@gmail.com>
2025-04-08 21:06:39 +0700

    add : starter absence app

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className=" font-bold text-red-400 underline">Ini merupakan starter aplikasi absensce tempe ayu</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        skyLight: "#C3E9FF",
+        skyDark: "#025F96",
+        Warm: "#F6F6F6",
+      },
+    },
+  },
+  plugins: [],
+};
