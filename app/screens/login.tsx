b023c60d7b13a1b1951f44dac63c36e43913db5b
rayhanizzudd <rayhannavis11@gmail.com>
2025-04-21 00:27:28 +0700

    add : form login and register

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,26 +1,40 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
+import { View, Text, TouchableOpacity, TextInput, Image } from "react-native";
+import React from "react";
+import { useRouter, Link } from "expo-router";
 
-const Login = () => {
-    const router = useRouter()
+const Register = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Login</Text>
-      <TouchableOpacity onPress={() => router.replace("./logregpage")}>
-        <Text>Kembali</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.replace("./logregpage")}>
-        <Text>Kembali</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.replace("./logregpage")}>
-        <Text>Kembali</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.replace("./logregpage")}>
-        <Text>Kembali</Text>
-      </TouchableOpacity>
+    <View className="flex-1 justify-center items-center px-4 bg-white">
+      <Image
+        source={require("./../../assets/images/logo.png")}
+        className="h-48 w-96 absolute top-10"
+        resizeMode="contain"
+      />
+      {/* <TouchableOpacity onPress={() => router.back()}>
+        <Text> kembali</Text>
+        </TouchableOpacity> */}
+      <Text className="text-3xl font-bold text-start">Masuk</Text>
+      <View className="absolute bottom-32  flex flex-col justify-center items-center gap-3 w-full p-4 rounded-xl">
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="Nama"
+        >
+          {/* <Text>nama</Text> */}
+        </TextInput>
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="password"
+        >
+          {/* <Text>no password</Text> */}
+        </TextInput>
+        <TouchableOpacity className="bg-lime-500 w-2/5 rounded-xl px-4 py-2 mt-10">
+          <Link href="/beranda" className="text-lg font-semibold text-center">Masuk</Link>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
-export default Login
+export default Register;
--- app/screens/register.tsx
+++ app/screens/register.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native'
 import React from 'react'
 import { useRouter, Link } from "expo-router";
 
@@ -6,26 +6,45 @@ const Register = () => {
     const router = useRouter()
 
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text>Register</Text>
-      <TouchableOpacity onPress={() => router.back()}>
+    <View className="flex-1 justify-center items-center px-4 bg-white">
+      <Image
+                    source={require("./../../assets/images/logo.png")}
+                    className="h-48 w-96 absolute top-10"
+                    resizeMode="contain"
+                  />
+      {/* <TouchableOpacity onPress={() => router.back()}>
         <Text> kembali</Text>
-      </TouchableOpacity>
-      <View className="border-1 rounded-xl border-black px-6 py-3 ">
-        <Text>nama</Text>
+        </TouchableOpacity> */}
+        <Text className='text-3xl font-bold text-start'>Daftar Akun</Text>
+      <View className="absolute bottom-32  flex flex-col justify-center items-center gap-3 w-full p-4 rounded-xl">
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="Nama"
+        >
+          {/* <Text>nama</Text> */}
+        </TextInput>
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="no telephone"
+        >
+          {/* <Text>no telephone</Text> */}
+        </TextInput>
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="password"
+        >
+          {/* <Text>no password</Text> */}
+        </TextInput>
+        <TextInput
+          className="border-2 rounded-xl border-black px-6 py-3 w-full"
+          placeholder="konfirmasi password"
+        >
+          {/* <Text>konfirmasi password</Text> */}
+        </TextInput>
+        <TouchableOpacity className="bg-lime-500 w-2/5 rounded-xl px-4 py-2 mt-10">
+          <Text className="text-lg font-semibold text-center">Daftar</Text>
+        </TouchableOpacity>
       </View>
-      <View className="border-1 rounded-xl border-black px-6 py-3 ">
-        <Text>no telephone</Text>
-      </View>
-      <View className="border-1 rounded-xl border-black px-6 py-3 ">
-        <Text>no password</Text>
-      </View>
-      <View className="border-2 rounded-xl border-black px-6 py-3 ">
-        <Text>konfirmasi password</Text>
-      </View>
-      <TouchableOpacity className="bg-lime-500 rounded-xl px-4 py-2 ">
-        <Text className="text-lg font-semibold">Daftar</Text>
-      </TouchableOpacity>
     </View>
   );
 }
